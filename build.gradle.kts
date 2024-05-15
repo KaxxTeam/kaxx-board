f338033de3860d83dde79853f2e861329c89f9a8
Mathys St-Jean-Rioux <98367059+mstjr@users.noreply.github.com>
2024-05-15 16:11:28 -0400

    Update Java source compatibility to Java 17
    An update has been made to the build.gradle.kts file changing Java source compatibility from version 1.8 to version 17. This impacts how the code of this project gets compiled.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ group = "ca.kaxx"
 version = "0.0.1"
 
 java{
-    sourceCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
     withJavadocJar()
     withSourcesJar()
 }
